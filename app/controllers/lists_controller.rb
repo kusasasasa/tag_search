f78e56f12a3d3bae217323f63571ef5aaded4389
EC2 Default User <ec2-user@ip-172-31-23-26.ap-northeast-1.compute.internal>
2023-03-26 10:23:18 +0000

    Initial Commit

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,22 @@
 class ListsController < ApplicationController
   def new
+    @list=List.new
   end
-
+  
   def index
   end
 
   def show
+    @list
   end
-
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to '/top'
+  end
   def edit
+  end
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
